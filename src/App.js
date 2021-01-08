8198b5c789214a6d24a6422bc2863552b3f307a8
jm717 <jm717@jm717>
2021-01-08 00:02:25 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Lookup from './Lookup.js'
+
+function App() {
+  return (
+
+    <div className="App">
+      <h2> Application coming soon </h2>
+      <h4> We are working on delivering a high quality webpage, Hold on ...</h4>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Lookup.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function Lookup() {
+
+  return (
+      <input placeholder="search movie" />
+    )
+}
+
+
+export default Lookup;
